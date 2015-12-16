56ba308f022bb10c78971fe04a45ecc759dce520
sickachu <inuksirhc@Gmail.com>
2015-12-15 16:20:07 -0800

    Undid last change, only verifying if user put a negative value for total number of elements. Additional min remains. C++ exit calls are added.
    Signed-off-by: sickachu <inuksirhc@Gmail.com>

--- maxofasequence.cpp
+++ maxofasequence.cpp
@@ -10,18 +10,31 @@
 
 using namespace::std;
 
-int verifyEntry(int entry)//on outside, let a return of 0 mean a quit. pass integer to indicate what value we are checking? (i.e. if we are checking for length, width, etc)
+int verifyEntry(int entry)//on outside, let a return of 0 mean a quit. This is only verifying the number of total elements
 {
 	if(entry == 0)
 	{
 		return 0;
 	}
-
+	if(entry < 0)
+	{
+		cout << "Entry invalid, please enter a non-zero, non-negative value (enter 0 to exit): " ;
+		cin >> entry;
+		while(entry < 0)
+		{
+			cout << "Entry invalid, please enter a non-zero, non-negative value (enter 0 to exit): ";
+			cin >> entry;
+			if(entry == 0)
+			{
+				break;
+			}
+		}
+		return entry;
+	}
 }
 int main()
 {
-	int max,next,min = numeric_limits<int>::min();
-	int n = 0;
+	int max,next,min,n = numeric_limits<int>::min();
 	int counter = 1;
 	int entry;
 	
@@ -30,7 +43,10 @@ int main()
 	cout << "(enter 0 to exit) n = ";
 	cin >> entry;
 	n = verifyEntry(entry);
-	
+	if(n == 0)
+	{
+		exit(0);
+	}
 	cout << "Now, enter each element of the sequence. If you made a mistake with" << endl;
 	cout << "the total number of elements, simply put a number smaller than the " << endl;
 	cout << "smallest number in the sequence (i.e. if the minumum is 1, then " << endl;
@@ -54,6 +70,7 @@ int main()
 	
 	cout << "max is " << max << ". Min is " << min << ". Double strike enter to exit this prompt." << endl;
 	system("PAUSE");
+	exit(0);
 }
 	
 
